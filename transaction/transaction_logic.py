5ec84ca8713d96a7e1088fccf456d649f68bc284
isatyamks <isatyamks@gmail.com>
2024-09-04 14:31:49 +0530

    feat: Add Transaction class with signature verification

---
+++ transaction/transaction_logic.py
@@ -0,0 +1,20 @@
+class Transaction:
+    def __init__(self, sender, recipient, amount):
+        self.sender = sender
+        self.recipient = recipient
+        self.amount = amount
+
+    def to_dict(self):
+        return {"sender": self.sender, "recipient": self.recipient, "amount": self.amount}
+
+    def sign_transaction(self, private_key):
+        transaction_data = f"{self.sender}{self.recipient}{self.amount}".encode()
+        self.signature = private_key.sign(transaction_data)
+
+    def verify_transaction(self, public_key):
+        transaction_data = f"{self.sender}{self.recipient}{self.amount}".encode()
+        try:
+            public_key.verify(self.signature, transaction_data)
+            return True
+        except:
+            return False
