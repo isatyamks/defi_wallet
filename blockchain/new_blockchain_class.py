8c16e4f6830d484ba405c4fbf4fdd1bb745cbaac
isatyamks <isatyamks@gmail.com>
2024-09-07 16:35:47 +0530

    chore: Refactor blockchain implementation and add validation logic

--- blockchain/new_blockchain_class.py
+++ blockchain/new_blockchain_class.py
@@ -1,5 +1,10 @@
-import
-class blockchain:
+import datetime as date
+
+import hashlib
+
+
+
+class block:
         def __init__(self,index,timestamp,data,previous_hash):
             self.index = index
             self.timestamp= timestamp
@@ -14,7 +19,43 @@ class blockchain:
 
 
 
+# class blockchain:
+
+#     def __ init__self():
+#         self.chain = [self.create_genesis_block()]
+
+#         def create_genesis_block(self):
+#             return block(,date.datetime.now(),"Genesis Block","0")
+
+#         def get_latest_block
 
 
 
 
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+
+    def create_genesis_block(self):
+        return block(0, date.datetime.now(), "Genesis Block", "0")
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, new_block):
+        new_block.previous_hash = self.get_latest_block().hash
+        new_block.hash = new_block.calculate_hash()
+        self.chain.append(new_block)
+
+    def is_valid(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i-1]
+
+            if current_block.hash != current_block.calculate_hash():
+                return False
+
+            if current_block.previous_hash != previous_block.hash:
+                return False
+
+        return True
