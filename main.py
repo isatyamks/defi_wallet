a0074717acd7f746e8e7eaa3e5797e3d7cce8881
isatyamks <isatyamks@gmail.com>
2024-09-07 16:31:20 +0530

    Refactor main.py to use new_blockchain_class and add sample blocks

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
-from wallet.wallet_key import generate_keys
-from transaction.transaction_logic import Transaction
-from blockchain.blockchain_class import Block,Blockchain
+#from wallet.wallet_key import generate_keys
+#from transaction.transaction_logic import Transaction
+#from blockchain.blockchain_class import Block,Blockchain
+import datetime as date
 
 
 
-x,y = generate_keys()
 
-print (x)
-print(y)
+from blockchain.new_blockchain_class import block,Blockchain
+
+# x,y = generate_keys()
+
+# print (x)
+# print(y)
+
+
+
+
+blockchain = Blockchain()
+
+
+blockchain.add_block(block(1, date.datetime.now(),"Transaction Data 1",""))
+blockchain.add_block(block(2, date.datetime.now(),"Transaction Data 2",""))
+blockchain.add_block(block(3, date.datetime.now(),"Transaction Data 3",""))
+
+
+for block in block.chain:
+    print("Block #"+str(block.index))
+    print("Timestamp:"+str(block.Timestamp))
+    print("Data: "+block.data)
+    print("Hash: "+block.hash)
+    print("Previous Hash: "+block.previous_hash)
+    print("\n")
+
 
