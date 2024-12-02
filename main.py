42c316218c2916a615815be9ea694e852815b913
isatyamks <isatyamks@gmail.com>
2024-12-02 09:34:54 +0530

    feat: Update blockchain implementation to include new block addition and display logic

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #from wallet.wallet_key import generate_keys
 #from transaction.transaction_logic import Transaction
-#from blockchain.blockchain_class import Block,Blockchain
+from blockchain.blockchain_class import Block,Blockchain
 import datetime as date
 
 
-
-
+from blockchain.chat_block_link import block,Blockchain
+ 
 from blockchain.new_blockchain_class import block,Blockchain
 
 # x,y = generate_keys()
@@ -13,23 +13,43 @@ from blockchain.new_blockchain_class import block,Blockchain
 # print (x)
 # print(y)
 
+def block(self,transaction,date,previous_hash,hash):
+
+
+
+# blockchain = Blockchain()
+
+
+# blockchain.add_block(block(1, date.datetime.now(),"Transaction Data 1",""))
+# blockchain.add_block(block(2, date.datetime.now(),"Transaction Data 2",""))
+# blockchain.add_block(block(3, date.datetime.now(),"Transaction Data 3",""))
+# blockchain.add_block(block(4, date.datetime.now(),"Transaction Data 3",""))
+# blockchain.add_block(block(5, date.datetime.now(),"Transaction Data 3",""))
+
+
+# for block in block.chain:
+#     print("Block #"+str(block.index))
+#     print("Timestamp:"+str(block.Timestamp))
+#     print("Data: "+block.data)
+#     print("Hash: "+block.hash)
+#     print("Previous Hash: "+block.previous_hash)
+#     print("\n")
+
+
 
 
 
 blockchain = Blockchain()
+blockchain.add_block(block(1, date.datetime.now(), "Block 1 Data", blockchain.get_latest_block().hash))
+blockchain.add_block(block(2, date.datetime.now(), "Block 2 Data", blockchain.get_latest_block().hash))
 
 
-blockchain.add_block(block(1, date.datetime.now(),"Transaction Data 1",""))
-blockchain.add_block(block(2, date.datetime.now(),"Transaction Data 2",""))
-blockchain.add_block(block(3, date.datetime.now(),"Transaction Data 3",""))
-
-
-for block in block.chain:
-    print("Block #"+str(block.index))
-    print("Timestamp:"+str(block.Timestamp))
-    print("Data: "+block.data)
-    print("Hash: "+block.hash)
-    print("Previous Hash: "+block.previous_hash)
+for block in blockchain.chain:
+    print("\n\n\n")
+    print("Block #" + str(block.index))
+    print("Timestamp: " + str(block.timestamp))
+    print("Data: " + block.data)
+    print("Hash: " + block.hash)
+    print("Previous Hash: " + block.previous_hash)
     print("\n")
-
 
